b1919d879306a6cb304d27517e0cc964eb9b7d70
Lily-Mbugua <61009265+Lily-Mbugua@users.noreply.github.com>
2020-08-11 20:44:14 +0300

    Add files via upload

---
+++ Servo_Motor_Code/Servo_Motor_Code.ino
@@ -0,0 +1,16 @@
+include <servo.h>
+servo myservo;
+
+int PotPin =0;
+int Val;
+void setup (){
+  myservo.attach(a); 
+  }
+
+void loop(){
+
+val =analogRead(PotPin);
+val =map(val,011023, 0, 180);
+myservo.write(val);
+delay(15)
+  }
